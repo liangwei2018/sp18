e3302f85bc0c095531ae93c9100b25e4ebd812ba
Liang Wei <liang@Liangs-Air.T-mobile.com>
2018-12-21 21:26:29 -0600

    lab1 checkLeapYear completed

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,8 +1,15 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Liang Wei
  */
 public class LeapYear {
-
+    /** Check whether or not a year is a Leap Year
+     *  Return true if it's a Leap Year and false otherwise
+     */
+    public static boolean isLeapYear(int year){
+        if(year % 400 ==0 || (year %4==0 && year %100!=0) )
+            return true;
+        return false;
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
