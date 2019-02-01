93f9b83fb0810163f7779f906e4e4951281ad981
Liang Wei <liang@Liangs-Air.T-mobile.com>
2019-02-01 14:55:56 -0600

    Access modifier update for class Planet in the proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,12 +6,12 @@
  */
 
 public class Planet {
-    double xxPos;
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    private double xxPos;
+    private double yyPos;
+    private double xxVel;
+    private double yyVel;
+    private double mass;
+    private String imgFileName;
 
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img) {
